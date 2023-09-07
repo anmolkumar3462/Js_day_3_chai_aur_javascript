30de723da134827328a60f879689386412a5050d
anmolkumar3462 <132343530+anmolkumar3462@users.noreply.github.com>
2023-09-07 09:34:05 +0000

    container config done

---
+++ 01_basics/data_types.js
@@ -0,0 +1,21 @@
+//In this class we will learn about data types
+//So there are so many data types in javascript
+//For example
+
+//In javascript data types are classified into two ways
+//1. Primitive data types
+    // Some primitive data types are
+    //1. Number data
+    //2. Big Int
+    //3. String
+    //4. Boolean (True or False)
+    //5. null
+    //6. undefined
+
+//2. Non Primitive data types
+    //1. Object
+
+// There is a way through which we can identify the data types which is Type of 
+console.log(typeof true);
+
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+// console.log("Hello Anmol!")
+let studentId = "Anmol";
+let email = "abc@gmail.com";
+const password = "12345";
+const city = "Samastipur";
+// There is an another way to create a variable which is without let,var and const and that is without anything that means you just have to write variable name and value. that's it..
+// but this method is not recommended
+pincode = "848101";
+
+//now we are going to change the value 
+
+studentId = "Anmol kumar";
+email = "abc123@gmail.com";
+// password = "abc@123";
+// city = "SAMASTIPUR";
+
+console.table([studentId,email,password,city]);
