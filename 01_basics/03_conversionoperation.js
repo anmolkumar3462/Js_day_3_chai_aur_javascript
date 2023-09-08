3694bc2e5830e4813bf07b8927450abbdd8117eb
anmolkumar3462 <132343530+anmolkumar3462@users.noreply.github.com>
2023-09-08 06:45:33 +0000

    data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,16 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
